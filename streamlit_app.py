5c4a509ca3843dc88f280021f0cb25ae3557bb8d
Himanshu Verma <himanshu.verma@impetus.com>
2023-02-16 22:18:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
 
+import streamlit
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free Ranged Egg')
+streamlit.text('Avacado Toast')
+streamlit.header('Build Your Own Fruit Smoothie')
